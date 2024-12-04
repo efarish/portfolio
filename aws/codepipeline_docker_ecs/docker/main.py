aae2a4fa308390de7af25513a41981152f8a1bd1
enriqueelgato <eric.farish@live.com>
2024-12-04 15:37:11 -0500

    Bug fix 2 in endpoint main.py

--- aws/codepipeline_docker_ecs/docker/main.py
+++ aws/codepipeline_docker_ecs/docker/main.py
@@ -28,7 +28,7 @@ async def upload_file(file: UploadFile = File(...)):
     contents = await file.read()
     print(f"File {file.filename} saved at S3 bucket: {S3_BUCKET}")
     s3.put_object(Bucket=S3_BUCKET, Key=file.filename, Body=contents)
-    response_json = f'{"filename":"{file.filename}", "bucket":"{S3_BUCKET}" }'
+    response_json = f'{{"filename":"{file.filename}", "bucket":"{S3_BUCKET}" }}'
     return Response(content=response_json, media_type="application/json")
 
 
