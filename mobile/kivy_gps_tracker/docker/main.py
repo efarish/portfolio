72433d4866cf663b17a739fd5b901095cb6b5df8
enriqueelgato <eric.farish@live.com>
2024-12-18 16:32:47 -0500

    Continue development.

--- mobile/kivy_gps_tracker/docker/main.py
+++ mobile/kivy_gps_tracker/docker/main.py
@@ -1,11 +1,15 @@
-import os
-
+import model
+from db import engine
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from routers import auth
 from starlette import status
 
+load_dotenv()
+
 app = FastAPI()
+
+model.Base.metadata.create_all(bind=engine)
 
 @app.get('/', status_code=status.HTTP_200_OK)
 def health_check():
--- mobile/kivy_gps_tracker/docker/model.py
+++ mobile/kivy_gps_tracker/docker/model.py
@@ -1,2 +1,11 @@
 from db import Base
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_name = Column(String, unique=True)
+    password = Column(String)
+    role = Column(String)
