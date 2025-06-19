cc80149d71a0310c40c3e4d25274c55abdd8fb80
efarish <eric.farish@live.com>
2025-06-19 07:40:08 -0400

    Added script to build docker image locally.

---
+++ llm/rag_ondemand/docker/build_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PROJECT_NAME=rag_ondemand # Used build path to source files.
+ECR_REPO=ecs1                 # Name of ECR image repository.
+DOCKER_IMAGE_NAME=rag_ondemand     # Name of Tracker Docker image.
+#ECR_MAIN_URI="${AWS_ACCT}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+ECR_IMG_NAME=${ECR_REPO}:${DOCKER_IMAGE_NAME}
+#ECR_IMAGE_URI=${ECR_MAIN_URI}/${ECR_REPO}:${DOCKER_IMAGE_NAME}
+
+#aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_MAIN_URI}
+docker build -t ${ECR_IMG_NAME} ./
+#aws cloudformation package --template ./llm/${PROJECT_NAME}/cloudformation/create-app/template.yaml --s3-bucket a-unique-artifact-bucket-name --output-template-file packaged-template.yaml
+#docker tag ${ECR_IMG_NAME} ${ECR_IMAGE_URI}
+#docker push ${ECR_IMAGE_URI}
+
+#docker container run -p 80:80 $ECR_IMG_NAME
